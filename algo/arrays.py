155acee41b1476b4891fe407c158b532b4317d90
Emily Yu <emilyyst19@hotmail.com>
2024-09-01 00:07:51 -0400

    Update implementation of top k elements

--- algo/arrays.py
+++ algo/arrays.py
@@ -7,14 +7,18 @@ def topk(arr: list, k: int) -> list:
         k: number of elements to return
     
     Returns:
-        list: top k smallest elements
+        list: top k smallest elements, not sorted
+    
+    Runtime: O(n log k)
+    
+    Implementation: uses a maxheap to maintain the top k smallest elements
     """
 
     n = len(arr)
     heap = []
     for i in range(n):
-        heapq.heappush(heap, arr[i])
-    ans = []
-    for _ in range(k):
-        ans.append(heapq.heappop(heap)[1])
-    return ans
+        heapq.heappush(heap, -arr[i])
+        if len(heap) > k:
+            heapq.heappop(heap)
+            
+    return [-x for x in heap]
